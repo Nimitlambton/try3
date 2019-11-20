7b51dfecd8a6760dbade046ae0510e4f2eff71ab
JASI <jasimac@jasis-mac.local>
2019-11-19 19:28:50 -0800

    Initial Commit

---
+++ try3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  try3
+//
+//  Created by JASI on 19/11/19.
+//  Copyright © 2019 Nimit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
